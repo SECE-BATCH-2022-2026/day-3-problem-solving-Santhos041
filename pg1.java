36f3747838f6a5563dfd53a7aeb64e5b92808136
Santhos041 <136429362+Santhos041@users.noreply.github.com>
2025-12-05 14:11:18 +0530

    pg1

--- pg1.java
+++ pg1.java
@@ -1,1 +1,28 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int count = UserMainCode.countOccurrance(s1, s2);
+        System.out.println(count);
+    }
+}
+
+class UserMainCode {
+    public static int countOccurrance(String s1, String s2) {
+        int count = 0;
+
+        for (int i = 0; i <= s1.length() - s2.length(); i++) {
+            int j = 0;
+            while (j < s2.length() && s1.charAt(i + j) == s2.charAt(j)) {
+                j++;
+            }
+            if (j == s2.length()) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
 
